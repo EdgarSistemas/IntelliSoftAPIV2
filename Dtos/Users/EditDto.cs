b3b43f0bff8b88808d5b56796302b826cef3ce01
IDGS-904-20001559 <20001559@alumnos.utleon.edu.mx>
2025-08-07 22:41:25 -0600

    Se agrega nueva funcionalidad recuperar contrasena

---
+++ Dtos/Users/EditDto.cs
@@ -0,0 +1,11 @@
+﻿namespace IntelliSoftAPIV2.Dtos.Users
+{
+    public class EditDto
+    {
+        public string Id { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+
+    }
+
+}
